36b38d33321d8049e7bc8f1b0a406d815c7ebb3d
James Priest <james-priest@outlook.com>
2019-01-16 06:50:42 -0800

    fix: remove console; remove css body height

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class ListContacts extends Component {
   render() {
-    // console.log('Props', this.props);
     return (
       <ol className="contact-list">
         {this.props.contacts.map(contact => (
